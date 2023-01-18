fdf3ab672e68ea1c07123f151726c383e27feb2a
ARYAMEHR ABBASPOOR <aryamehrabbaspoor@gmail.com>
2023-01-18 14:58:54 +0330

    question-2

--- 2.py
+++ 2.py
@@ -3,10 +3,10 @@ def heh(x):
     for i in range(0,len(x)-1):
         if x[i] == x[i+1]:
             count += 1
-    print(count)
+    return count
 
 
 inputString = input("Enter: ")
 
 
-heh(inputString)
+print(heh(inputString))
